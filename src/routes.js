e85b9f9dc6902fdf35788dd1687a0a9ee4b02903
perseul <perseulmeira@gmail.com>
2023-12-08 17:06:42 -0300

    add integration test on http requests

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,10 @@ const routes = express.Router();
 const productsController = require("./controllers/productsController");
 const pedidosController = require('./controllers/pedidosController')
 
+
 routes.post("/pedidos",pedidosController.store);
 
-routes.get("/products", productsController.index)
+routes.get("/products", productsController.index);
 routes.get("/products/:id", productsController.show);
 routes.post("/products", productsController.store);
 routes.put("/products/:id", productsController.update);
